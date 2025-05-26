90a98c15264659fd6d11ae689a0b84df76ac3821
swb-buildarchetype <sam@buildarchetype.dev>
2025-05-25 23:06:10 -0400

    Add separate line echo statements.

--- .buildkite/build-base-image.mjs
+++ .buildkite/build-base-image.mjs
@@ -16,8 +16,10 @@ const pipeline = {
       command: [
         "export TART_REGISTRY_USERNAME=\"$GITHUB_USERNAME\"",
         "export TART_REGISTRY_PASSWORD=\"$GITHUB_TOKEN\"",
-        "echo \"TART_REGISTRY_USERNAME: $TART_REGISTRY_USERNAME\"",
-        "echo \"TART_REGISTRY_PASSWORD: $TART_REGISTRY_PASSWORD\"",
+        "echo TART_REGISTRY_USERNAME",
+        "echo $TART_REGISTRY_USERNAME",
+        "echo TART_REGISTRY_PASSWORD",
+        "echo $TART_REGISTRY_PASSWORD",
         "tart login ghcr.io",
         "chmod +x .buildkite/scripts/ensure-bun-image.sh",
         ".buildkite/scripts/ensure-bun-image.sh 2>&1 | tee -a base-image-build.log",
