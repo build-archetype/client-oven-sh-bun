d56731fe4f90525fb039d05a0bf805561868df02
swb-buildarchetype <sam@buildarchetype.dev>
2025-06-02 13:28:13 -0400

    Add buildkite true flag.

--- .buildkite/ci.mjs
+++ .buildkite/ci.mjs
@@ -412,6 +412,7 @@ function getBuildEnv(target, options) {
   const revision = typeof canary === "number" ? canary : 1;
 
   return {
+    BUILDKITE: "true",
     ENABLE_BASELINE: baseline ? "ON" : "OFF",
     ENABLE_CANARY: revision > 0 ? "ON" : "OFF",
     CANARY_REVISION: revision,
